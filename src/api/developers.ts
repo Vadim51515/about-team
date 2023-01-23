7fe61b43464fc61348d513e28fb62c90f71fc09c
Evgeny <selivanukev@gmail.com>
2023-01-23 19:25:10 +0500

    fake api

---
+++ src/api/developers.ts
@@ -0,0 +1,11 @@
+export const developers = [
+  {
+    id: 1,
+    name: "Евгений Селиванюк",
+    role: "Front-end developer",
+    description: "Best of the best 😎",
+    img: "",
+    projects: [1],
+    skills: [1, 2, 3, 4, 5, 6, 8],
+  },
+];
---
+++ src/api/skills.ts
@@ -0,0 +1,42 @@
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+  },
+  {
+    id: 3,
+    name: "SCSS",
+  },
+  {
+    id: 4,
+    name: "JS",
+  },
+  {
+    id: 5,
+    name: "TS",
+  },
+  {
+    id: 6,
+    name: "React",
+  },
+  {
+    id: 7,
+    name: "Android",
+  },
+  {
+    id: 8,
+    name: "IOS",
+  },
+  {
+    id: 9,
+    name: "UE",
+  },
+  {
+    id: 10,
+    name: "Python",
+  },
+];
